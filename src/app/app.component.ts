6f8fd4d2e6991ec8fb942e8a9d5cb0fcdd73495e
sonu <sonu.prashant@thermofisher.com>
2020-08-02 23:34:00 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { EChartOption } from "echarts";
 import { GetDataService } from "./services/get-data.service";
 import { HttpClient } from "@angular/common/http";
-import { ValueConverter } from "@angular/compiler/src/render3/view/template";
 
 @Component({
   selector: "app-root",
@@ -11,13 +10,8 @@ import { ValueConverter } from "@angular/compiler/src/render3/view/template";
 })
 export class AppComponent implements OnInit {
   title = "timeseries-assignment";
-  private limit = 5;
-  private barChartLimit = 2;
   dataLoaded = false;
-  pieChartLoaded = false;
   barChartOption: EChartOption;
-  pieChartOption: EChartOption;
-  chartOptions: EChartOption[] = [];
 
   constructor(
     private getDataService: GetDataService,
@@ -25,171 +19,81 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.httpClient.get<any[]>("assets/final.json").subscribe(data => {
+    this.httpClient.get("assets/delhi_weather_data.txt", 
+    {responseType: 'text'}).subscribe(data => {
       this.getDataService.getData(data).subscribe(res => {
-        this.renderBarChart(res[1], res[0]);
-        this.dataLoaded = true;
+        this.rendeLineChartOption(res);
       });
     });
   }
 
-  onLimitChange(value) {
-    this.limit = value;
-  }
-
-  onLimitChangeOfBar(value) {
-    this.barChartLimit = value;
-  }
-
-  loadBarChart() {
-    this.chartOptions = [];
-    this.getDataService
-      .getBarData(this.barChartLimit, "location")
-      .subscribe(data => {
-        data.forEach(element => {
-          let option = this.getBarChartOption(
-            element.xAxisData,
-            element.yAxisData,
-            element.attrValue
-          );
-          this.chartOptions.push(option);
-        });
-      });
-  }
-
-  loadPieChart() {
-    this.getDataService.getPieData(this.limit).subscribe(data => {
-      this.renderPieChart(data);
-      this.pieChartLoaded = true;
+  rendeLineChartOption(myData) {
+    let dateList = myData.map(function (item) {
+        return item[2];
     });
-  }
+    let dayValueList = myData.map(function (item) {
+        return item[4];
+    });
 
-  renderPieChart(gdata) {
-    this.pieChartOption = {
-      title: {
-        text: "Pie Chart",
-        subtext: "Attribute distribution",
-        left: "center",
-        top: 10
-      },
-      tooltip: {
-        trigger: "item",
-        formatter: function(params) {
-          const tipInfo = params["data"];
-          console.log(tipInfo.toolTipData);
-          let list = tipInfo.toolTipData;
-          let html = "<ul>";
-          list.forEach(element => {
-            const section = element.section.slice(0, 5) + ".....";
-            html +=
-              "<li> Document : " +
-              element.document +
-              " and in sections = " +
-              section +
-              "</li>";
-          });
+    let nightValueList = myData.map(function (item) {
+      return item[5];
+    });
 
-          html = html + "</ul>";
-          return (
-            `Attribute = ${tipInfo.name} <br/> : Total count = ${tipInfo.value}  <br/>
-          ` + html
-          );
-        }
-      },
-      legend: {
-        type: "scroll",
-        orient: "vertical",
-        right: 90,
-        top: 50,
-        bottom: 20,
-        data: gdata.legendData
-      },
-      series: [
-        {
-          top: 50,
-          name: "Attribute",
-          type: "pie",
-          radius: "75%",
-          center: ["50%", "50%"],
-          data: gdata.seriesData,
-          emphasis: {
-            itemStyle: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: "rgba(0, 0, 0, 0.5)"
-            }
-          }
-        }
-      ]
-    };
-  }
-
-  renderBarChart(xAxisData, yAxisData) {
     this.barChartOption = {
       title: {
-        text: "Bar Chart",
-        subtext: "Time series graph ",
-        left: "center"
-      },
-      xAxis: {
-        name: "Year of publition",
+        text: 'Weather Report'
+    },
+    toolbox: {
+      feature: {
+          saveAsImage: {
+          }
+      }
+    },
+    tooltip: {
+      trigger: "axis"
+    },
+    legend: {
+        data: ['Day Temp', 'Night Temp']
+    },
+    grid: {
+        bottom: '20%',
+        containLabel: true
+    },
+    dataZoom: [
+      {
+          show: true,
+          realtime: true,
+          start: 0,
+          end: 100,
+      }
+    ],
+    xAxis: [{
+        data: dateList,
+        name: "Months (Jan 2019 - July 2020)",
         nameLocation: "middle",
-        nameGap: 25,
+        nameGap: 35,
         nameTextStyle: { color: "black", fontWeight: "bold" },
-        type: "category",
-        data: yAxisData
-      },
-      yAxis: {
-        name: "No of published pages",
-        type: "value",
-        nameLocation: "middle",
-        nameGap: 50,
-        nameTextStyle: { color: "black", fontWeight: "bold" }
-      },
-      series: [
+    }
+    ],
+    yAxis: {
+      name: "Temperature in °C",
+      type: "value",
+      nameLocation: "middle",
+      nameGap: 50,
+      nameTextStyle: { color: "black", fontWeight: "bold" },
+    },
+    series: [
+        {   
+            name: 'Day Temp',
+            type: 'line',
+            data: dayValueList
+        },
         {
-          top: 50,
-          center: ["50%", "50%"],
-          data: xAxisData,
-          type: "bar",
-          areaStyle: {}
+            name: 'Night Temp',
+            type: 'line',
+            data: nightValueList
         }
-      ]
+    ]
     };
-  }
-
-  getBarChartOption(xAxisData, yAxisData, value) {
-    let chartOption: EChartOption = {
-      title: {
-        text: "Attribute = " + value,
-        subtext: "Attribute value distribution",
-        left: "center"
-      },
-      xAxis: {
-        name: "Year of publition",
-        nameLocation: "middle",
-        nameGap: 25,
-        nameTextStyle: { color: "black", fontWeight: "bold" },
-        type: "category",
-        data: yAxisData
-      },
-      yAxis: {
-        name: "No of documents value present",
-        type: "value",
-        nameLocation: "middle",
-        nameGap: 50,
-        nameTextStyle: { color: "black", fontWeight: "bold" }
-      },
-      series: [
-        {
-          top: 50,
-          center: ["50%", "50%"],
-          data: xAxisData,
-          type: "bar",
-          areaStyle: {}
-        }
-      ]
-    };
-    return chartOption;
   }
 }
--- src/app/services/get-data.service.ts
+++ src/app/services/get-data.service.ts
@@ -1,242 +1,28 @@
-import { Injectable, OnInit } from "@angular/core";
-import { WikiData } from "../model/data";
-import { Observable, BehaviorSubject, EMPTY } from "rxjs";
+import { Injectable } from "@angular/core";
+import { Observable, BehaviorSubject } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class GetDataService {
-  private _wikiData = new BehaviorSubject<any>({});
-  private _pieData = new BehaviorSubject<any>([]);
-  private _barData = new BehaviorSubject<any>([]);
-  dataList: WikiData[] = [];
+  private _lineData = new BehaviorSubject<any>({});
+  dataList: any[] = [];
+  lines: any;
 
   constructor() {}
 
   getData(data): Observable<any> {
-    this.dataList = data;
-    const v = this.getAxisData();
-    this._wikiData.next(v);
-    return this._wikiData;
+    this.lines = data;
+    const v = this.getLineChartData();
+    this._lineData.next(v);
+    return this._lineData;
   }
 
-  getAxisData() {
-    this.dataList.sort((a, b) => {
-      const date1 = new Date(a.time_published);
-      const date2 = new Date(b.time_published);
-      return date1.getFullYear() - date2.getFullYear();
-    });
-
-    const xAxisData: number[] = [];
-    const YAxisData: number[] = [];
-    let map = new Map<number, number>();
-
-    this.dataList.forEach(wikiData => {
-      const date = new Date(wikiData.time_published);
-      const year = date.getFullYear();
-      if (map.has(year)) {
-        map.set(year, map.get(year) + 1);
-      } else {
-        map.set(year, 1);
-      }
-    });
-
-    map.forEach((key, value) => {
-      xAxisData.push(value);
-      YAxisData.push(key);
-    });
-    return [xAxisData, YAxisData];
-  }
-
-  getPieData(limit: number): Observable<any> {
-    let map = new Map<String, number>();
-    let mapLegend = new Map<String, any[]>();
-    let documentIndex = 0;
-    let prevIndex;
-    this.dataList.forEach(WikiData => {
-      const sec = WikiData.sections;
-      let secIndex = 0;
-      sec.forEach(section => {
-        section.attributes.forEach(attr => {
-          if (attr.values !== undefined) {
-            const valuesList = attr.values;
-            valuesList.forEach(attr => {
-              const key = attr.name.toLowerCase().trim();
-              if (
-                key !== "1" &&
-                key !== "2" &&
-                key !== "3" &&
-                key !== "first" &&
-                key !== "last" &&
-                key !== "pages" &&
-                key !== "page"
-              ) {
-                const input = {
-                  document: WikiData.title,
-                  section: [secIndex]
-                };
-                if (prevIndex === undefined) {
-                  map.set(key, 1);
-                  mapLegend.set(key, [input]);
-                } else if (mapLegend.has(key) && documentIndex === prevIndex) {
-                  map.set(key, map.get(key) + 1);
-                  const info = mapLegend.get(key);
-                  const i = info.length - 1;
-                  const sect = info[i]["section"];
-                  sect.push(secIndex);
-                  info[i]["section"] = sect;
-                  mapLegend.set(key, info);
-                } else if (mapLegend.has(key) && documentIndex !== prevIndex) {
-                  map.set(key, map.get(key) + 1);
-                  const info = mapLegend.get(key);
-                  const i = info.length;
-                  info[i] = input;
-                  mapLegend.set(key, info);
-                } else {
-                  map.set(key, 1);
-                  mapLegend.set(key, [input]);
-                }
-                prevIndex = documentIndex;
-              }
-            });
-          }
-        });
-        secIndex++;
-      });
-      documentIndex++;
-    });
-    const sortedMap = new Map(
-      [...map.entries()].sort((a, b) => {
-        return b[1] - a[1];
-      })
-    );
-    limit = Math.min(limit, sortedMap.size);
-
-    let index = 0;
-    let legendD = [];
-    let seriesD = [];
-    for (let key of sortedMap.keys()) {
-      const v = sortedMap.get(key);
-      seriesD.push({
-        name: key,
-        value: v,
-        toolTipData: mapLegend.get(key)
-      });
-      legendD.push(key);
-      if (++index === Number(limit)) {
-        break;
-      }
+  getLineChartData() {
+    for (const line of this.lines.split(/[\r\n]+/)){
+      const v = line.split(",");
+      this.dataList.push(v);
     }
-    const pieData = {
-      legendData: legendD,
-      seriesData: seriesD
-    };
-
-    this._pieData.next(pieData);
-    return this._pieData;
-  }
-
-  getBarData(limit: number, input: string): Observable<any> {
-    let map = new Map<string, number>();
-    let mapDate = new Map<string, string[]>();
-    let documentIndex = 0;
-    let prevIndex = 0;
-    this.dataList.forEach(WikiData => {
-      const sec = WikiData.sections;
-      sec.forEach(section => {
-        section.attributes.forEach(attr => {
-          if (attr.values !== undefined && documentIndex !== prevIndex) {
-            const valuesList = attr.values;
-            valuesList.forEach(attr => {
-              const key = attr.name.toLowerCase().trim();
-              const value = attr.value.trim();
-              if (key === input) {
-                const date = new Date(
-                  WikiData.time_published
-                ).toLocaleDateString();
-                if (map.has(value)) {
-                  map.set(value, map.get(value) + 1);
-                  let list = mapDate.get(value);
-                  list.push(date);
-                  mapDate.set(value, list);
-                } else {
-                  map.set(value, 1);
-                  mapDate.set(value, [date]);
-                }
-              }
-            });
-          }
-        });
-        prevIndex = documentIndex;
-      });
-      documentIndex++;
-    });
-
-    const sortedMap = new Map(
-      [...map.entries()].sort((a, b) => {
-        return b[1] - a[1];
-      })
-    );
-
-    let index = 0;
-    let topValues = [];
-    for (let key of sortedMap.keys()) {
-      topValues.push(key);
-      if (++index === Number(limit)) {
-        break;
-      }
-    }
-
-    let datas = [];
-
-    topValues.forEach(data => {
-      let map = new Map<string, number>();
-      let dates = mapDate.get(data);
-      dates.forEach(date => {
-        if (map.has(date)) {
-          map.set(date, map.get(date) + 1);
-        } else {
-          map.set(date, 1);
-        }
-      });
-      const sortedMap = new Map(
-        [...map.entries()].sort((a, b) => {
-          let res1 = a[0].split("/");
-          let res2 = b[0].split("/");
-          if (Number(res1[2]) !== Number(res2[2])) {
-            return Number(res1[2]) - Number(res2[2]);
-          } else if (Number(res1[1]) !== Number(res2[1])) {
-            return Number(res1[1]) - Number(res2[1]);
-          } else if (Number(res1[0]) !== Number(res2[0])) {
-            return Number(res1[0]) - Number(res2[0]);
-          } else {
-            return 0;
-          }
-        })
-      );
-
-      let xAxis = [];
-      let yAxis = [];
-
-      for (let key of sortedMap.keys()) {
-        const v = sortedMap.get(key);
-        xAxis.push(v);
-        yAxis.push(key);
-      }
-
-      datas.push({
-        xAxisData: xAxis,
-        yAxisData: yAxis,
-        attrValue: data
-      });
-    });
-    this._barData = new BehaviorSubject<any>([]);
-    this._barData.next(datas);
-    return this._barData;
-  }
-
-  addDays(date: Date, days: number): Date {
-    date.setDate(date.getDate() + days);
-    return date;
+    return this.dataList;
   }
 }
