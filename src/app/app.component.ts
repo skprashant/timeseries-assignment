5dee8a3eadd6c5de82e2de14714a81734a5da7c4
sonu <sonu.prashant@thermofisher.com>
2020-08-08 00:45:28 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,6 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
-  monthMap = {"January": 0, 
-              "February" :1,
-            "March" : 2,
-          'April': 3,
-        "May": 4,
-      "June": 5, "July": 6, "August": 7, "September": 8, "October" : 9,
-    "November": 10, "December": 12}
   title = "timeseries-assignment";
   dataLoaded = false;
   barChartOption: EChartOption;
